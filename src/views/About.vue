67cf76ff863f544ab030c9adaa4a173e22dc1ef7
Matt Thomas <mthomas@guggenheim.org>
2018-12-21 11:43:41 -0500

    Revert setting variable in view

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,14 +2,8 @@
 import DataProvider from '@/mixins/DataProvider';
 
 export default {
-  data() {
-    return {
-      first: 1,
-    };
-  },
   mixins: [DataProvider],
   mounted() {
-    this.first = this.getRandomInt(10);
     this.getData('posts');
   },
   name: 'about',
